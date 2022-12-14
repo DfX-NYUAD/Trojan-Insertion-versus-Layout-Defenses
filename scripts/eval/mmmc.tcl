90513fb26c8560cbbfbcf442e7f9b0977fac8a02
Johann Knechtel <jk176@b134-l-os22-ee.abudhabi.nyu.edu>
2022-12-14 12:23:48 +0400

    benchmarks/_release/_scripts updates to latest versions; minor update note scripts/eval/mmmc.tcl

--- scripts/eval/mmmc.tcl
+++ scripts/eval/mmmc.tcl
@@ -30,7 +30,7 @@ create_delay_corner -name TT_delay_corner \
     -late_rc_corner TT_rc_corner
 
 ## constraint_mode
-# NOTE INTERNAL ONLY: the latency.sdc file is essential for meeting timing according to constraints in original PD run
+# NOTE the latency.sdc file is essential for checking timing according to constraints set in original PD run
 create_constraint_mode -name TT_constraints \
     -sdc_files { design.sdc latency.sdc }
 
