e5ce05cdf4937cedc0ee361e6ba6e9d02c6f11f3
Johann Knechtel <jk176@b134-l-os22-ee.abudhabi.nyu.edu>
2023-01-26 09:42:57 +0400

    scripts/gdrive/ISPD23_helper_team_emails.sh refactored; only essential parts from initialize()

--- scripts/gdrive/ISPD23_helper_team_emails.sh
+++ scripts/gdrive/ISPD23_helper_team_emails.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 source ISPD23_daemon.settings
-source ISPD23_daemon_procedures.sh
 
-initialize "production"
+echo "Team emails"
+echo "==========="
 
-echo ""
+while read -r a b; do
+	google_team_folders[$a]=$b
+done < <(./gdrive list --no-header -q "parents in '$google_root_folder' and trashed = false and not (name contains '_test')" | awk '{print $1" "$2}')
 
 emails_string_all=""
 
@@ -13,7 +15,8 @@ emails_string_all=""
 for google_team_folder in "${!google_team_folders[@]}"; do
 
 	team=${google_team_folders[$google_team_folder]}
-	team_=$(printf "%-"$teams_string_max_length"s" $team)
+
+	google_share_emails[$team]=$(./gdrive share list $google_team_folder | tail -n +2 | awk '{print $4}' | grep -Ev "$emails_excluded_for_notification" | grep '@')
 
 	team_emails=${google_share_emails[$team]}
 
@@ -23,10 +26,11 @@ for google_team_folder in "${!google_team_folders[@]}"; do
 		emails_string+="$email; "
 	done
 
-	echo "Team $team_ : $emails_string"
+	echo "Team $team : $emails_string"
 
 	emails_string_all+=$emails_string
 done
+echo ""
 
 echo "ALL : $emails_string_all"
 echo ""
