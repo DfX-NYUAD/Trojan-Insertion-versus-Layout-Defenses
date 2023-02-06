eef5c0cfc6b4b1bd05c6faf5379986a063019174
Johann Knechtel <jk176@b134-l-os22-ee.abudhabi.nyu.edu>
2023-02-06 13:33:43 +0400

    scripts/eval/ranking/ranking.sh updates

--- scripts/eval/ranking/ranking.sh
+++ scripts/eval/ranking/ranking.sh
@@ -1,77 +1,72 @@
 #!/bin/bash
 
-##################
-# settings
-##################
+######################
+# 0) settings, init
+######################
+
+list_rpts_default=1
+team_real_name_default=1
+score_related_OVERALL_default=1
+settings="../../gdrive/ISPD23_daemon.settings"
 
 if [ $# -lt 1 ]; then
 	echo "Parameters required:"
-	echo "1) Score -- pick from the following: des_issues OVERALL des_perf des des_area fsp_fi_ea_n ti_sts fsp_fi_ea_c ti_fts fsp_fi ti des_p_total"
+	echo "1) Metric -- pick one from those available in the scores.rpt files, e.g., OVERALL"
 	echo "Parameters optional:"
-	echo "2) List related rpt files -- 0: no; 1: yes -- default is 1"
-	echo "3) Team names -- 0: anonymous label; 1: actual names -- default is 0"
-	echo "4) Include blind benchmarks -- 0: no; 1: yes -- default is 0"
-	echo "5) Score related to OVERALL -- 0: no, means best score is selected independent of OVERALL score; 1: yes, means best score is extracted for run w/ best OVERALL score -- default is 1"
+	echo "2) List related rpt files -- 0: no; 1: yes -- default is $list_rpts_default"
+	echo "3) Team names -- 0: anonymous label; 1: actual names -- default is $team_real_name_default"
+	echo "4) Score related to OVERALL -- 0: no, means that best score is selected (for the metric of choice) independently of OVERALL score; 1: yes, means that best score (for the metric of choice) is extracted for run w/ best OVERALL score -- default is $score_related_OVERALL_default"
+	echo "5) Path for ISPD23_daemon.settings file -- default is $settings"
 	exit
 fi
 
-round="final"
-local_root_folder="$HOME/ISPD22"
-teams_root_folder="$local_root_folder/data/$round"
-baselines_root_folder="$local_root_folder/benchmarks/__release/__$round"
-
-benchmarks="AES_1 AES_2 AES_3 Camellia CAST MISTY openMSP430_1 PRESENT SEED TDEA"
-benchmarks_blind="openMSP430_2 SPARX"
-
-# associative array w/ teams still in final round but the same anon team ID from alpha round
+# associative array w/ teams and anon IDs
 declare -A teams
-teams[CUEDA]=A
-teams[NTUsplace]=E
-teams[Seo]=J
-teams[TalTech]=K
-teams[TCLAB]=L
-teams[UT_pda]=N
-teams[XDSecurity]=O
-teams[DASYS]=Q
+teams[_test]=A
 
+# other settings
 ND="--------"
-grep_term="$1:"
+metric="$1"
 
+# tmp log file; used for keeping track of best runs
+rpts=/tmp/$metric"_"$(date +%s).log
+
+# call parameters 
 if [[ $2 != "" ]]; then
 	list_rpts=$2
 else
-	list_rpts=1
+	list_rpts=$list_rpts_default
 fi
 
 if [[ $3 != "" ]]; then
 	team_real_name=$3
 else
-	team_real_name=0
+	team_real_name=$team_real_name_default
 fi
 
 if [[ $4 != "" ]]; then
-	include_blind_benchmarks=$4
+	score_related_OVERALL=$4
 else
-	include_blind_benchmarks=0
+	score_related_OVERALL=$score_related_OVERALL_default
 fi
 
 if [[ $5 != "" ]]; then
-	score_related_OVERALL=$5
+	source $5
 else
-	score_related_OVERALL=1
+	source $settings
 fi
 
-##################
+echo "Parameters:"
+echo " Metric: $1"
+echo " List related rpt files: $list_rpts"
+echo " Team names: $team_real_name"
+echo " Score related to OVERALL: $score_related_OVERALL"
+echo " Path for ISPD23_daemon.settings file: $settings"
+echo
 
-if [[ $include_blind_benchmarks == 1 ]]; then
-	benchmarks=$benchmarks" $benchmarks_blind"
-fi
-
-rpts=/tmp/$grep_term"_"$(date +%s).rpts
-
-######
-# 1) obtain stats
-######
+######################
+# 1) obtain scores
+######################
 
 echo "Extracting scores ..."
 
@@ -79,7 +74,7 @@ declare -A scores_baseline
 for benchmark in $benchmarks; do
 
 	## NOTE baseline is baseline; here we don't need to differentiate b/w best score related to OVERALL or not
-	scores_baseline[$benchmark]=$(grep -w "$grep_term" $baselines_root_folder/$benchmark/reports/scores.rpt | tail -n 1 | awk '{print $2}')
+	scores_baseline[$benchmark]=$(grep -w "$metric" $baselines_root_folder/$benchmark/reports/scores.rpt | tail -n 1 | awk '{print $NF}')
 done
 
 # iterate over keys
@@ -87,7 +82,7 @@ for team in "${!teams[@]}"; do
 
 	# init dedicated arrays for each team
 
-#	echo "Extracting min scores for team $team ..."
+	echo " Extracting best scores (i.e., min value) for team $team ..."
 
 	## some characters are not supported for bash var; replace them
 	team_=$(echo $team | sed -e 's/-/_/g')
@@ -117,13 +112,18 @@ for team in "${!teams[@]}"; do
 			fi
 
 			## skip runs w/o scores
-			if ! [[ -e $backup_work/$run/scores.rpt ]] ; then
+			if ! [[ -e $backup_work/$run/reports/scores.rpt ]] ; then
+				continue
+			fi
+
+			## also skip runs w/ errors
+			if [[ -e $backup_work/$run/reports/errors.rpt ]] ; then
 				continue
 			fi
 
 			## extract scores
-			score_OVERALL_curr=$(grep -w "OVERALL" $backup_work/$run/scores.rpt | tail -n 1 | awk '{print $2}')
-			score_curr=$(grep -w "$grep_term" $backup_work/$run/scores.rpt | tail -n 1 | awk '{print $2}')
+			score_OVERALL_curr=$(grep -w "OVERALL" $backup_work/$run/reports/scores.rpt | tail -n 1 | awk '{print $NF}')
+			score_curr=$(grep -w "$metric" $backup_work/$run/reports/scores.rpt | tail -n 1 | awk '{print $NF}')
 
 			## track lowest scores across runs
 
@@ -140,7 +140,7 @@ for team in "${!teams[@]}"; do
 				score_OVERALL_min=$score_OVERALL_curr
 
 				# also log the rpt file
-				echo $backup_work/$run/scores.rpt >> $rpts
+				echo $backup_work/$run/reports/scores.rpt >> $rpts
 			fi
 			## NOTE in this mode, the actual min score should only be initialized when also the OVERALL min score is already initialized
 			if [[ $score_related_OVERALL == 1 ]]; then
@@ -151,7 +151,7 @@ for team in "${!teams[@]}"; do
 						eval $array_name[$benchmark]=$score_curr
 
 						# also log the rpt file
-						echo $backup_work/$run/scores.rpt >> $rpts
+						echo $backup_work/$run/reports/scores.rpt >> $rpts
 
 						continue
 					fi
@@ -164,7 +164,7 @@ for team in "${!teams[@]}"; do
 					eval $array_name[$benchmark]=$score_curr
 
 					# also log the rpt file
-					echo $backup_work/$run/scores.rpt >> $rpts
+					echo $backup_work/$run/reports/scores.rpt >> $rpts
 
 					continue
 				fi
@@ -178,6 +178,7 @@ for team in "${!teams[@]}"; do
 				# NOTE in this mode, we want to check for any solution with at least as best OVERALL score, not only better ones (the latter would likely exclude submissions that improve on actual score)
 				if (( $(echo "$score_OVERALL_curr <= $score_OVERALL_min" | bc -l) )); then
 
+#					# dbg
 #					echo $run
 #					echo " $score_OVERALL_curr <= $score_OVERALL_min"
 
@@ -185,12 +186,14 @@ for team in "${!teams[@]}"; do
 
 					## update actual score for improvements of actual score itself or for overall score (for the latter case we would reset the actual score)
 					if (( $(echo "$score_curr < $score_min" | bc -l) || $(echo "$score_OVERALL_curr < $score_OVERALL_min" | bc -l))); then
+
+#						# dbg
 #						echo "  $score_curr <= $score_min"
 
 						eval $array_name[$benchmark]=$score_curr
 
 						# also log the rpt file
-						echo $backup_work/$run/scores.rpt >> $rpts
+						echo $backup_work/$run/reports/scores.rpt >> $rpts
 					fi
 				fi
 
@@ -203,46 +206,50 @@ for team in "${!teams[@]}"; do
 					eval $array_name[$benchmark]=$score_curr
 
 					# also log the rpt file
-					echo $backup_work/$run/scores.rpt >> $rpts
+					echo $backup_work/$run/reports/scores.rpt >> $rpts
 				fi
 			fi
 
+#			# dbg
 #			echo "Benchmark $benchmark -- score_curr: $score_curr; score_min: $(eval echo \${$array_name[$benchmark]})"
 		done
 	done
 
 #	echo "Done"
+
 done
 echo "Done"
 echo
 
-######
-# 2) print stats
-######
+######################
+# 2) print scores
+######################
 
 ## 1st row: header
-echo -n "Team/Benchmark	"
-echo -n "Baseline	"
+out=""
+out+="Benchmark "
+#out+="Baseline "
 for team in "${!teams[@]}"; do
 
 	# real or anon team name
 	if [[ $team_real_name == 1 ]]; then
-		echo -n "$team	"
+		out+="$team "
 	else
-		echo -n "${teams[$team]}	"
+		out+="${teams[$team]} "
 	fi
 done
 # end row
-echo
+# NOTE see https://stackoverflow.com/a/3182519 for newline handling
+out+=$'\n'
 
 ## build up rows, one per benchmark
 for benchmark in $benchmarks; do
 
 	# 1st col: benchmark name
-	echo -n "$benchmark	"
+	out+="$benchmark "
 
-	# 2nd col: baseline scores
-	echo -n "${scores_baseline[$benchmark]}	"
+#	# 2nd col: baseline scores
+#	out+="${scores_baseline[$benchmark]} "
 
 	# remaining cols: team scores
 	for team in "${!teams[@]}"; do
@@ -252,19 +259,60 @@ for benchmark in $benchmarks; do
 		array_name="scores_$team_"
 
 		scores_string=$(eval echo \${$array_name[$benchmark]})
-		echo -n "$scores_string	"
+		out+="$scores_string "
 	done
 	# end row
-	echo
+	out+=$'\n'
 done
 
+# print as table
+# NOTE quotes are required here for proper newline handling (https://stackoverflow.com/a/3182519)
+echo "$out" | column -t
+
+######################
+# 3) print files
+######################
+
 if [[ $list_rpts == 1 ]]; then
+
 	echo
-	echo "Underlying rpt files/runs:"
-	## also print the underlying rpt files
-	for benchmark in $benchmarks; do
-		for team in "${!teams[@]}"; do
-			cat $rpts | grep $benchmark | grep $team | tail -n 1
-		done
+	echo "Underlying report files:"
+	echo
+
+	out=""
+	out+="Benchmark "
+	for team in "${!teams[@]}"; do
+	
+		# real or anon team name
+		if [[ $team_real_name == 1 ]]; then
+			out+="$team "
+		else
+			out+="${teams[$team]} "
+		fi
 	done
+	# end row
+	# NOTE see https://stackoverflow.com/a/3182519 for newline handling
+	out+=$'\n'
+
+	## build up rows, one per benchmark
+	for benchmark in $benchmarks; do
+	
+		# 1st col: benchmark name
+		out+="$benchmark "
+	
+		# remaining cols: underyling rpt file
+		for team in "${!teams[@]}"; do
+
+			out+=$(cat $rpts | grep $benchmark | grep $team | tail -n 1)
+		done
+		# end row
+		out+=$'\n'
+	done
+	
+	# print as table
+	# NOTE quotes are required here for proper newline handling (https://stackoverflow.com/a/3182519)
+	echo "$out" | column -t
 fi
+
+# cleanup
+rm $rpts
