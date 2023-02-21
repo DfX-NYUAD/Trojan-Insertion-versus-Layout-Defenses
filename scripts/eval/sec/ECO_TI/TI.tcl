afcb896d259dd710a47668286fc6724d65040953
Johann Knechtel <jk176@b134-l-os22-ee.abudhabi.nyu.edu>
2023-02-21 14:20:28 +0400

    scripts/eval/sec/ECO_TI/TI.tcl don't put DRC reports into reports/ as sharing those w/ teams would reveal maybe too much about Trojan

--- scripts/eval/sec/ECO_TI/TI.tcl
+++ scripts/eval/sec/ECO_TI/TI.tcl
@@ -35,12 +35,13 @@ ecoRoute
 
 #####################
 # post-insertion checks
-#####################
 #
 # NOTE other checks in checks.tcl are skipped here, since 1) accounting for all them during scoring seems difficult and, more importantly, 2) these checks were more to try to catch
 # any cheating/trivial defenses
+#####################
 #
-verify_drc -limit 100000 -report reports/$design_name.geom.$trojan_name.rpt
+# NOTE related report file is placed directly in the work dir, not in reports/ -- this is on purpose, as we don't want to share related reports/details to participants
+verify_drc -limit 100000 -report $design_name.geom.$trojan_name.rpt
 
 # simultaneous setup, hold analysis
 # NOTE applicable for (faster) timing analysis, but not for subsequent ECO runs or so -- OK for our scope here, after actual ECO commands
